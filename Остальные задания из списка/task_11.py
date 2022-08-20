6b6065e0fa06f264d4cf195a5a6c74838eb050b2
Lev Sobolenkov <dangerrrleo@gmail.com>
2022-08-20 13:41:31 +0300

    Добавлены 11 и 13 задание

---
+++ Остальные задания из списка/task_11.py
@@ -0,0 +1,10 @@
+n_enter = int(input("Введите N: "))
+# num_start = int(input("Введите число с которого начнется последовательность: "))
+emperty_list = []
+i = 0
+while i < n_enter:
+    emperty_list.append((-3)**i)
+    i += 1
+print(emperty_list)
+
+
